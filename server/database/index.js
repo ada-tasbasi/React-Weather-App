00f780d0b83ef8e595275681df130191df8a2ee1
Bespolezhny <ayt95@hotmail.com>
2019-06-04 01:43:11 +0300

    Update backend for deployment

--- server/database/index.js
+++ server/database/index.js
@@ -1,5 +1,4 @@
 const {Pool} = require('pg'),
-      password; //psql password
 
 const CONNECTION_STRING = process.env.DATABASE_URL || 'postgresql://postgres:catharsis9@localhost:5432/weather-db',
       SSL = process.env.NODE_ENV === 'production';
